e59b2a46bc85f95af993a45db051ed0ba0d94d33
Ali Ihsan Turkmenoglu <aliihsanturk@gmail.com>
2018-10-14 22:45:25 +0300

    Odevler First Commit

---
+++ 2_sayilarin_carpimi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	printf("Carpmak istediginiz sayilari aralara bosluk koyarak giriniz: ");
+	//TO DO
+	//Take numbers from user
+	//Read & Save the numbers
+	//Multiply them
+
+}
