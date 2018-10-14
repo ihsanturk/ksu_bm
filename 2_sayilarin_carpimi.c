3f0eb614ee49b09c58fd1158a3adb0f44ecbc267
Ali Ihsan Turkmenoglu <aliihsanturk@gmail.com>
2018-10-14 22:55:51 +0300

    Edited some files

--- 2_sayilarin_carpimi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Carpmak istediginiz sayilari aralara bosluk koyarak giriniz: ");
-	//TO DO
-	//Take numbers from user
-	//Read & Save the numbers
-	//Multiply them
-
-}
