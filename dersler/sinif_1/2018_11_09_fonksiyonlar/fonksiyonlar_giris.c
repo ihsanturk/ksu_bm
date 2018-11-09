3942a6d5be5178105c33f7eb8f2dde4122217380
Ali Ihsan Turkmenoglu <aliihsanturk@gmail.com>
2018-11-09 10:30:40 +0300

    Added fonksiyonlar

---
+++ dersler/sinif_1/2018_11_09_fonksiyonlar/fonksiyonlar_giris.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+/* Fonksiyon Tanimlama
+ *
+ * <fonksiyon turu/tipi> <fonksiyon_adi> (<parametreler>) {
+ *	[kodlar...]
+ *	[kodlar...]
+ *	[kodlar...]
+ * }
+ *
+ * Fonksiyonu Kullanma
+ *
+ * fonksiyon_adi() {
+ *	
+ * }
+ *
+ * Fonksiyon Turleri
+ * 
+ * Eger fonksiyon bir deger return etmeyecekse fonksiyon turu olarak "void" kullanilir.
+ * Eger fonksiyon bir int degeri return edecekse fonksiyon turu olarak "int" kullanilir.
+ * vb.
+ * 
+ * Fonkiyon prototipleri main fonksiyonunun ustune tanimlanabilir. Bu sayede tanimlanacak fonksiyonlar main fonksiyounun
+ * altina tanimlanabilir.
+ */
+
+int main () {
+}
