e2c95f8966dee8bdc3cdc4993116c2e1608ff0cc
Ali Ihsan Turkmenoglu <aliihsanturk@gmail.com>
2018-12-13 09:59:50 +0300

    Added new files

--- dersler/sinif_1/2018_11_09_fonksiyonlar/fonksiyonlar_giris.c
+++ dersler/sinif_1/2018_11_09_fonksiyonlar/fonksiyonlar_giris.c
@@ -20,8 +20,11 @@
  * Eger fonksiyon bir int degeri return edecekse fonksiyon turu olarak "int" kullanilir.
  * vb.
  * 
+ * Notlar
+ *
  * Fonkiyon prototipleri main fonksiyonunun ustune tanimlanabilir. Bu sayede tanimlanacak fonksiyonlar main fonksiyounun
- * altina tanimlanabilir.
+ * altina tanimlanabilir. Dolayisiyla main fonksiyonu dosyanin baslangicinda direk gorulebilir. Aksi takdirde
+ * main fonksiyonunu gormek icin kendi yazdigimiz fonksiyonlarin asagisina inmemiz gerekecekti.
  */
 
 int main () {
