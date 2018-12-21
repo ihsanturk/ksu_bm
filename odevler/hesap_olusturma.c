bc0eccf3d510db08ff3477404998d7dd1a39d81b
ihsanturk <aliihsanturk@gmail.com>
2018-12-21 12:09:01 +0300

    Added new files

---
+++ odevler/hesap_olusturma.c
@@ -0,0 +1,5 @@
+/* Kullanici adi sadece harflerden olusabilir
+ * Kullanici adi minimum 5 harften olusacak
+ * Sifre harf rakam karisik olacak
+ * Sifre minimum 7 karakter olacak
+ * Kullanicinin adini ve sifresini alarak dogrulugunu kontrol et
