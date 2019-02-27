7e9f446ab9f55b1e98ac24bfc2da7f641f96306a
ihsanturk <aliihsanturk@gmail.com>
2019-02-27 14:25:25 +0300

    update

---
+++ dersler/sinif_1_donem_2/2019_02_20_structurelar/[yarim]structurelar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//Structures
+typedef struct
+{
+    char cinsiyet;
+    char ad[30];
+    char soyad[30];
+    int yas;
+} yolcu;
+
+//Main
+int main(int argc, char* argv[])
+{
+    yolcu y1;
+
+    y1.cinsiyet = 'e';
+    y1.ad = "Hasan";
+    y1.soyad = "Badem";
+    y1.yas = 1;
+}
