233637ed7b79a947e0fa0f64406130fffe8c503b
ihsanturk <aliihsanturk@gmail.com>
2019-03-14 15:38:28 +0300

    update

--- persembe_uygulamasi/kutu_sinifi.cpp
+++ persembe_uygulamasi/kutu_sinifi.cpp
@@ -11,28 +11,29 @@ public:
   // Constructor
   Kutu(int y, int g, int d) {
     yukseklik = y;
-    genislik = g;
-    derinlik = d;
+    genislik  = g;
+    derinlik  = d;
   }
+
 
   // + Operatoru
   Kutu operator+(Kutu &x) {
     return Kutu(yukseklik + x.yukseklik, 
-               genislik  + x.genislik,
-               derinlik  + x.derinlik);
+                genislik  + x.genislik,
+                derinlik  + x.derinlik);
   }
 
   // = Operatoru
   void operator=(Kutu &x) {
     yukseklik = x.yukseklik; 
-    genislik = x.genislik;
-    derinlik = x.derinlik;
+    genislik  = x.genislik;
+    derinlik  = x.derinlik;
   }
 
   // Yaz fonksiyonu
   void yaz() {
-    std::cout << this->yukseklik << ", " 
-              << this->genislik  << ", "
+    std::cout << this->yukseklik << ", "        // Yukseklik, genislik ve derinlik degiskenleri
+              << this->genislik  << ", "        // private oldugu icin "this->" komutunu kullandim.
               << this->derinlik  << std::endl;
   }
 };
